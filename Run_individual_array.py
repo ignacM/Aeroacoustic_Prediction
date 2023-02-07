ac6e6780c1a860ea214f9ce1569210199b52fefe
ignac <egyim2@nottingham.ac.uk>
2023-02-07 17:28:43 +0000

    Run_model.py now runs all the dataset. It also compares prediction of real vs predicted dB values by excluding a blade angle. Run_individual_array.py partial data.

---
+++ Run_individual_array.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib as plt
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+from tree_search import runmodel
+from decision_tree_final import runTree
+
+from sklearn.tree import export_graphviz
+
+
+path = 'Data\preliminary_data_2.xlsx'
+df = pd.read_excel(path)
+
+df_polar = df.iloc[:276, 1:]  # 115 we are omitting contra-rotation data, 276 for preldata2
+df_azimuthal = df.drop('db_pol', axis=1)
+df_azimuthal = df_azimuthal.iloc[:276, 0:5]
+
+Y_polar = df_polar.iloc[:, 0]
+X_polar = df_polar.iloc[:, 1:3]  # Three since we are omitting thrust
+
+Y_azi = df_azimuthal.iloc[:, 0]
+X_azi = df_azimuthal.iloc[:, 1:6]
+
+#runmodel(X_polar, Y_polar)
+runmodel(X_azi, Y_azi)
+
+
+
+"""
+Try GD like Neider Mead
+Minimizing the cost function
+Other methods like Bayesian optimization
+"""
--- Run_model.py
+++ Run_model.py
@@ -1,20 +1,48 @@
 import pandas as pd
-import matplotlib as plt
-from sklearn import preprocessing
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
+from tree_search import runmodel
 from decision_tree_final import runTree
+from sklearn.tree import DecisionTreeRegressor
+from decision_tree import print_actual_vs_real
 
 
-path = 'Data\preliminary_data_2.xlsx'
-df = pd.read_excel(path)
-
-df_polar = df.iloc[:115,1:]
-df_azimuthal = df.drop('db_pol', axis =1)
-
-Y_polar = df_polar.iloc[:,0]
-X_polar = df_polar.iloc[:, 1:]
-
-runTree(X_polar, Y_polar)
+def compare_trends(x_train, y_train, x_test, y_test):
+    tre = DecisionTreeRegressor(max_depth=8, min_samples_split=2, ccp_alpha=0.02)
+    tre.fit(x_train, y_train)
+    ypred = tre.predict(x_test)
+    print_actual_vs_real(y_test, ypred)
+    return
 
 
+if __name__ == '__main__':
+
+    path = 'Data\combined_data.xlsx'
+    df = pd.read_excel(path)
+    X_data = df.iloc[:253, 1:]
+    Y_data = df.iloc[:, 0]
+    # runmodel(X_data, Y_data)
+    # runTree(X_data, Y_data)
+    # best Tree parameters for combined data:
+    """tre = DecisionTreeRegressor(max_depth=8, min_samples_split=2, ccp_alpha=0.02)"""
+
+    exclude = 60
+
+    newX = X_data.drop(X_data.index[X_data['degree'] == exclude])
+    newY = df.drop(df.index[df['degree'] == exclude]).reset_index(drop=True)
+    newY = newY.iloc[:230, 0]
+    # 230 since its the half of the dataset
+    x_test = X_data.drop(X_data.index[X_data['degree'] != exclude])
+    y_test = df.drop(df.index[df['degree'] != exclude]).reset_index(drop=True)
+    y_test = y_test.iloc[:23, 0]
+
+    compare_trends(newX, newY, x_test, y_test)
+
+
+
+
+
+
+"""
+Try GD like Neider Mead
+Minimizing the cost function
+Other methods like Bayesian optimization
+"""
