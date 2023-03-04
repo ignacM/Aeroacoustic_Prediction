3218ff4efd66b3117ead29fe18d37826fb63a70c
ignac <egyim2@nottingham.ac.uk>
2023-03-04 00:26:41 +0000

    refactor to study and compare bayesian optimization & bayesian optimization with gaussian process

--- Run_model_2_GB.py
+++ Run_model_2_GB.py
@@ -6,12 +6,14 @@ from sklearn.tree import DecisionTreeRegressor
 from decision_tree import print_actual_vs_real
 from gradientbooster import run_model_variance, optimize
 from sklearn.ensemble import GradientBoostingRegressor
+import xgboost as xgb
 from sklearn.svm import SVR
-from skopt import gp_minimize
+from skopt import gp_minimize, BayesSearchCV
 from skopt import space
 from sklearn.model_selection import cross_val_score
 from sklearn import ensemble
 from skopt.utils import use_named_args
+from sklearn.pipeline import Pipeline
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, ConstantKernel, Exponentiation, RBF
@@ -19,12 +21,10 @@ from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, ConstantKe
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 if __name__ == '__main__':
-
     # Dividing data between X and Y
     path = 'Data\combined_data.xlsx'
     df_azi = pd.read_excel(path, sheet_name='azi')
     df_azi['exclude'] = 0
-
 
     exclude = 60
     df_azi.loc[(df_azi['degree'] == 60), 'exclude'] = 1
@@ -36,28 +36,31 @@ if __name__ == '__main__':
 
     x_train = X_data[(X_data[:, -1] == 0), 0:2]
     x_test = X_data[(X_data[:, -1] == 1), 0:2]
+
     y_train = Y_data.loc[(Y_data['exclude'] == 0), 'db']
     y_test = Y_data.loc[(Y_data['exclude'] == 1), 'db']
 
     # define a parameter space for GaussianProcessR:
-    param_space = [space.Integer(1, 5, name='max_depth'),
-          space.Real(10**-5, 10**0, "log-uniform", name='learning_rate'),
-          space.Integer(2, 100, name='min_samples_split'),
-          space.Integer(1, 100, name='min_samples_leaf'),
-          space.Categorical(['huber', 'quantile'], name='loss'),
-          space.Real(10**-5, 0.1,  name='alpha')]
+    param_space = [space.Integer(1, 15, name='max_depth'),
+                   space.Integer(100, 1500, name='n_estimators'),
+                   space.Real(10**-5, 10**0, "log-uniform", name='learning_rate'),
+                   space.Integer(2, 200, name='min_samples_split'),
+                   space.Integer(1, 100, name='min_samples_leaf'),
+                   space.Categorical(['huber', 'quantile'], name='loss'),
+                   space.Real(10**-5, 0.99,  name='alpha')]
 
-    param_names = ['max_depth', 'learning_rate', 'min_samples_split', 'min_samples_leaf', 'loss', 'alpha']
+    param_names = ['max_depth', 'n_estimators', 'learning_rate', 'min_samples_split', 'min_samples_leaf', 'loss', 'alpha']
     #Kernel = ConstantKernel(constant_value=1.4)
     #Kernel = Exponentiation(kernel=ConstantKernel(), exponent=1.1)
 
     @use_named_args(param_space)
     def optimize_2(**params):
         regressor = GradientBoostingRegressor(**params)
-        return -np.mean(cross_val_score(regressor, x_train, y_train, cv=5, n_jobs=-1, scoring="neg_mean_absolute_error"))
+        return -np.mean(cross_val_score(regressor, x_train, y_train, cv=3, n_jobs=-1, scoring="neg_mean_absolute_error"))
 
     # inside the main function
-    res_gp = gp_minimize(optimize_2, dimensions=param_space, n_calls=100, verbose=10, n_initial_points=20)
+    res_gp = gp_minimize(optimize_2, dimensions=param_space, n_calls=40, verbose=10, n_initial_points=40)
+
 
     best_parameters = dict(zip(param_names, res_gp.x))
     print(best_parameters)
@@ -70,9 +73,29 @@ if __name__ == '__main__':
         return
 
     compare_trends(x_train, y_train, x_test, y_test)
+    GBR_search = {
+        'model': space.Categorical([GradientBoostingRegressor()]),
+        'model__max_depth': space.Integer(1, 15, name='max_depth'),
+        'model__n_estimators': space.Integer(100, 1500, name='n_estimators'),
+        'model__min_samples_split': space.Integer(2, 100, name='min_samples_split'),
+        'model__min_samples_leaf': space.Integer(1, 100, name='min_samples_leaf'),
+        'model__loss': space.Categorical(['huber', 'quantile'], name='loss'),
+        'model__alpha': space.Real(10 ** -5, 0.99, name='alpha')
+    }
 
+    pipe = Pipeline([
+        ('model', GradientBoostingRegressor())
+    ])
+    res_bp = BayesSearchCV(pipe, [(GBR_search, 40)], cv=3)
+    res_bp.fit(x_train, y_train)
 
-
+    """best_parameters = dict(zip(param_names, res_bp.best_params_))
+    params = best_parameters[1:]"""
+    params = dict(res_bp.best_params_)
+    print(params)
+    best_parameters = params['model'].get_params()
+    print(type(best_parameters))
+    compare_trends(x_train, y_train, x_test, y_test)
 
 
 
