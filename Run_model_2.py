d0863d6fb7030ba9c4e31c5e63648acee8f7c554
ignac <egyim2@nottingham.ac.uk>
2023-03-04 00:21:27 +0000

    Found hyperparameters of SVM. Polar array: [C: 6-8, epsilon: 0.12-0.2] Azimuthal array: [C:35-70, epsilon: 0.15-0.3]

--- Run_model_2.py
+++ Run_model_2.py
@@ -17,29 +17,30 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, ConstantKernel, Exponentiation
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from skopt import BayesSearchCV
+from sklearn.pipeline import Pipeline
+
+from scipy.optimize import minimize
 
 if __name__ == '__main__':
 
     # Dividing data between X and Y
     path = 'Data\combined_data.xlsx'
-    df_azi = pd.read_excel(path, sheet_name='pol')
+    df_azi = pd.read_excel(path, sheet_name='azi')
     df_azi['exclude'] = 0
 
-
-    exclude = 60
-    df_azi.loc[(df_azi['degree'] == 60), 'exclude'] = 1
+    exclude = 135
+    df_azi.loc[(df_azi['degree'] == exclude), 'exclude'] = 1
 
     X_data = df_azi.drop(['db'], axis=1)
     scaler = MinMaxScaler()
-    X_data = scaler.fit_transform(X_data)
+    X_data= scaler.fit_transform(X_data)
     Y_data = df_azi.drop(['theta', 'degree'], axis=1)
 
     x_train = X_data[(X_data[:, -1] == 0), 0:2]
     x_test = X_data[(X_data[:, -1] == 1), 0:2]
-
     y_train = Y_data.loc[(Y_data['exclude'] == 0), 'db']
     y_test = Y_data.loc[(Y_data['exclude'] == 1), 'db']
-
 
     # define a parameter space for SVR:
     # https://uk.mathworks.com/help/stats/fitcsvm.html?s_tid=srchtitle#namevaluepairarguments
@@ -47,8 +48,8 @@ if __name__ == '__main__':
     """param_space = [space.Real(0.25, 10, name='C'),
                    space.Real(0.01, 1, name='epsilon'),
                    space.Categorical(['auto', 'scale'], name='gamma')]"""
-    param_space = [space.Real(65, 90, name='C'),
-                   space.Real(0.350, 0.500, name='epsilon'),
+    param_space = [space.Real(20, 70, name='C'),
+                   space.Real(0.15, 0.5, name='epsilon'),
                    space.Categorical(['auto', 'scale'], name='gamma')
                    ]
 
@@ -61,7 +62,7 @@ if __name__ == '__main__':
     @use_named_args(param_space)
     def optimize_2(**params):
         regressor = SVR(**params)
-        return -np.mean(cross_val_score(regressor, x_train, y_train, cv=5, n_jobs=-1, scoring="neg_mean_absolute_error"))
+        return -np.mean(cross_val_score(regressor, x_train, y_train, cv=3, n_jobs=-1, scoring="neg_mean_absolute_error"))
 
     # inside the main function
     res_gp = gp_minimize(optimize_2, dimensions=param_space, n_calls=50, verbose=10, n_initial_points=20)
@@ -80,6 +81,28 @@ if __name__ == '__main__':
     compare_trends(x_train, y_train, x_test, y_test)
 
 
+
+
+
+    SVR_search = {
+        'model': space.Categorical([SVR()]),
+        'model__C': space.Real(0.1, 100, name='C'),
+        'model__epsilon': space.Real(0.01, 0.9, name='epsilon'),
+        'model__gamma': space.Categorical(['auto', 'scale'], name='gamma'),
+        'model__tol': space.Real(0.00005, 0.001, name='tol')
+    }
+
+    pipe = Pipeline([
+        ('model', SVR())
+    ])
+    res_bp = BayesSearchCV(pipe, [(SVR_search, 40)], cv=3)
+    res_bp.fit(x_train, y_train)
+
+    params = dict(res_bp.best_params_)
+
+    best_parameters = params['model'].get_params()
+    print(best_parameters)
+    compare_trends(x_train, y_train, x_test, y_test)
 
 
 
