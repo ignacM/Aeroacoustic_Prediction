fixed variance and bias estimations & more readable code

gradientbooster.py
@@ -1,8 +1,6 @@
 from sklearn.ensemble import GradientBoostingRegressor
 from decision_tree import print_actual_vs_real
 from decision_tree import print_regression_solutions
-"""import tensorflow as tf
-import keras"""
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from mlxtend.evaluate import bias_variance_decomp
@@ -14,8 +12,12 @@ from sklearn.metrics import mean_absolute_error as mae
 from sklearn.model_selection import cross_val_score
 import numpy as np
 from sklearn.linear_model import SGDRegressor
+from sklearn import model_selection, metrics, ensemble
+from sklearn.model_selection import KFold
+from skopt import gp_minimize
+from skopt import space
 
-def runGBM(X, Y, parameters, model=GradientBoostingRegressor):
+def run_model_variance(X, Y, parameters, model=GradientBoostingRegressor):
     """
     Runs a regression task. parameters and model to be specified.
     :param X:
@@ -31,6 +33,11 @@ def runGBM(X, Y, parameters, model=GradientBoostingRegressor):
     parameters = parameters
 
     def mae_scores(arguments):
+        """
+        Takes arguments of a model and trains the model.
+        :param arguments: parameters of the regressor
+        :return:
+        """
 
         regressor = model(**arguments)
         scores = -1 * cross_val_score(regressor, x_train, y_train, cv=3,
@@ -46,36 +53,53 @@ def runGBM(X, Y, parameters, model=GradientBoostingRegressor):
             loss='mse', random_seed=123)"""
         return np.mean(scores), train_error, test_error
 
-
     def bias_variance(arguments):
+        """
+        Given model arguments, returns bias and variance of the model
+        :param arguments:
+        :return: loss, bias and variane of model with specific arguments
+        """
         regressor = model(**arguments)
         loss, bias, variance = bias_variance_decomp(
             regressor, x_train.values, y_train.values, x_test.values, y_test.values,
             loss='mse', random_seed=123)
         return loss, bias, variance
 
-    def parameter_loss_mae(start, end, step, parameter_name):
+    def parameter_loss_mae(start, end, step, parameter_name, *args):
         results = {}
         train_errors = {}
         test_errors = {}
         loss = {}
         bias = {}
         variance = {}
-        varray = np.arange(start, end, step*3)
 
+        # Selecting how often to produce variance / bias calculations. Selected to only calculate every 4 steps.
+        varray = np.arange(start, end, step*4)
         if type(start) == float:
             start = int(start*100)
             end = int(end*100)
             step = int(step*100)
             for i in range(start, end, step):
-                results[i/100], train_errors[i/100], test_errors[i/100] = mae_scores({parameter_name: i/100})
+                grid = {parameter_name: i / 100}
+                if not bool(args):  # Protective statement to add parameters to dictionary if dictionary is not empty
+                    grid.update(args)
+                results[i/100], train_errors[i/100], test_errors[i/100] = mae_scores(grid)
                 if i/100 in varray:
-                    loss[i / 100], bias[i / 100], variance[i / 100] = bias_variance({parameter_name: i/100})
+                    grid = {parameter_name: i/100}
+                    if not bool(args):
+                        grid.update(args)
+                    loss[i / 100], bias[i / 100], variance[i / 100] = bias_variance(grid)
         else:
             for i in range(start, end, step):
-                results[i], train_errors[i], test_errors[i] = mae_scores({parameter_name: i})
+                grid = {parameter_name: i}
+                if not bool(args):
+                    grid.update(args)
+                results[i], train_errors[i], test_errors[i] = mae_scores(grid)
                 if i in varray:
-                    loss[i], bias[i], variance[i] = bias_variance({parameter_name: i})
+                    grid = {parameter_name: i}
+                    if not bool(args):
+                        grid.update(args)
+                    loss[i], bias[i], variance[i] = bias_variance(grid)
 
 
         """ Plotting Parameter vs Mean Absolute Error"""
@@ -102,20 +126,44 @@ def runGBM(X, Y, parameters, model=GradientBoostingRegressor):
         plt.show()
         return test_errors
 
+
+    final_parameters = {}
     for i in range(0, len(parameters)):
         parameter_name = list(parameters)[i]
         start = list(parameters.values())[i][0]
         end = list(parameters.values())[i][1]
         step = list(parameters.values())[i][2]
 
-        test_errors = parameter_loss_mae(start, end, step, parameter_name)
+        test_errors = parameter_loss_mae(start, end, step, parameter_name, final_parameters)
         parameter_value = float(input('Where is the %s?' % parameter_name))
-        parameters[parameter_name] = parameter_value
 
-        """
-        After user input first parameter, then save that parameter and update list of parameters
-        then optimize next parameter and using the parameter that we have calculated
-        """
+        final_parameters[parameter_name] = parameter_value
+
+
 
     return
+
+def optimize(x, y, params, param_names):
+
+     # convert params to dictionary
+     parameters = dict(zip(param_names, params))
+
+     # initialize model with current parameters
+     # model = regressor(**parameters)
+     regressor = ensemble.GradientBoostingRegressor(**parameters)
+     lst_accu_stratified = []
+     """skf = KFold(n_splits=10, shuffle=True, random_state=1)
+     lst_accu_stratified = []
+
+     for train_index, test_index in skf.split(x, y):
+         x_train_fold, x_test_fold = x[train_index], x[test_index]
+         y_train_fold, y_test_fold = y[train_index], y[test_index]
+         regressor.fit(x_train_fold, y_train_fold)
+         lst_accu_stratified.append(regressor.score(x_test_fold, y_test_fold))"""
+     #regressor.fit(x, y)
+     #lst_accu_stratified.append(regressor.score(x, y))
+     # return negative accuracy
+     return -np.mean(cross_val_score(regressor, x, y, cv=5, n_jobs=-1,
+                                        scoring="neg_mean_absolute_error"))
+
 