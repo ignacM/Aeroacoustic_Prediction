4d8cb6a39c7bcb8678f61ab15edaf027496d99e7
ignacM <nacho.moral.a@gmail.com>
2023-03-07 23:54:20 +0000

    Created validation x_val data and plot graphs of predictions

--- src/validation.py
+++ src/validation.py
@@ -3,6 +3,7 @@ import numpy as np
 import joblib
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from train_test_split import deNormalize
+from functions import regression_eval as re
 
 
 
@@ -11,20 +12,36 @@ if __name__ == '__main__':
 
     # Load model:
     regressor = joblib.load('../models/SVM_regressor.pkl')
-    x_val = pd.read_csv('../Data/dec22/x_validation.csv')
+    df = pd.read_csv('../Data/dec22/x_validation.csv')
 
     # Scale data
     scaler = MinMaxScaler()
-    x_val = scaler.fit_transform(x_val)
+    df = scaler.fit_transform(df)
 
-    ypred = regressor.predict(x_val)
-    ypred = np.exp(ypred)
-    print(ypred)
+    predictions = []
+    predictions_name =\
+        [
+            '7.5 degrees', '22.5 degrees', '37.5 degrees', '52.5 degrees', '67.5 degrees', '82.5 degrees',
+            '97.5 degrees', '112.5 degrees', '127.5 degrees', '142.5 degrees', '157.5 degrees', '172.5 degrees'
+        ]
+    for degree in range(12):
+        a = degree*23
+
+        #x_val = np.array(x_val).reshape(-1, 1)
+        x_val = df[a:a + 23, [0, 1]]
+        print(x_val)
+
+        ypred = regressor.predict(x_val)
+        ypred = np.exp(ypred)
+        predictions.append(ypred)
 
 
 
+print('These are the preditcions:', predictions)
+print(len(predictions))
 
 
+re.print_predictions(predictions, predictions_name)
 
 
 
