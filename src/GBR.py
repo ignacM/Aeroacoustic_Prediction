79a25e7f772fca66505414fae56a5b6bf89c04a1
ignacM <nacho.moral.a@gmail.com>
2023-03-22 21:45:30 +0000

    Added working Gaussian Process Regressor trained model, and Boosted Tree Model

--- src/GBR.py
+++ src/GBR.py
@@ -2,7 +2,8 @@ import numpy as np
 import joblib
 
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.svm import SVR
+from sklearn.ensemble import GradientBoostingRegressor
+import sklearn.gaussian_process.kernels as kernels
 from sklearn.model_selection import cross_val_score
 
 from skopt import gp_minimize
@@ -17,37 +18,33 @@ if __name__ == '__main__':
 
     # Split data into train / test. Exclude Angle 60
     x_train, y_train, x_test, y_test = dataSplit(exclude=60, scaler=MinMaxScaler())
-    # SVR seemed to have the best performance when Min-max scaling of x data and log scale on y
+    # GPR seemed to have the best performance when Min-max scaling of x data and log scale on y
 
-    # define a parameter space for SVR:
-    """
-    C: Length of the margin. Distance between support points and margin.
-    epsilon: amount of points past the support points.
-    Gamma: radius of group similarity. Big gamma = points need to be very close to each other.
-    """
 
     # Define parameter names and space
-    param_names = ['C', 'epsilon', 'gamma', 'tol']
+
+    param_names = ['loss', 'learning_rate', 'n_estimators', 'max_depth', 'alpha']
 
     param_space = [
-        space.Real(0.001, 0.5, name='C'),
-        space.Real(0.0000000001, 0.005, name='epsilon'),
-        space.Categorical(['scale'], name='gamma'),
-        space.Real(0.01, 0.05, name='tol')
+        space.Categorical(['squared_error', 'absolute_error', 'huber', 'quantile'], name='loss'),
+        space.Real(0.00001, 0.1, name='learning_rate'),
+        space.Integer(10, 200, name='n_estimators'),
+        space.Integer(4, 6, name='max_depth'),
+        space.Real(0.00001, 0.1, name='alpha'),
     ]
 
     @use_named_args(param_space)
     def optimize_mae_function(**params):
         """
-        Fits an SVM regressor and returns an average of MAE over a number of cross validations
+        Fits an GP regressor and returns an average of MAE over a number of cross validations
         :param params:
         :return:
         """
-        regressor = SVR(**params)
+        regressor = GradientBoostingRegressor(**params)
         return -np.mean(cross_val_score(regressor, x_train, y_train, cv=3, n_jobs=-1, scoring="neg_mean_absolute_error"))
 
     # Use Bayesian Optimization with Gaussian process to find function minimum
-    res_gp = gp_minimize(optimize_mae_function, dimensions=param_space, n_calls=30, verbose=10, n_initial_points=20)
+    res_gp = gp_minimize(optimize_mae_function, dimensions=param_space, n_calls=40, verbose=10, n_initial_points=20)
 
     # Save parameters fround in global minimum
     best_parameters = dict(zip(param_names, res_gp.x))
@@ -64,7 +61,7 @@ if __name__ == '__main__':
         :return: descaled y_train, descaled ypred, and fitted model
         """
         # Fitting regressor with best parameters from optimization
-        regressor = SVR(**best_parameters)
+        regressor = GradientBoostingRegressor(**best_parameters)
         regressor.fit(x_train, y_train)
         # Predicting data
         ypred = regressor.predict(x_test)
@@ -80,31 +77,12 @@ if __name__ == '__main__':
 
     y_test, ypred, final_model = compare_trends(x_train, y_train, x_test, y_test)
 
-    plot_regression_outcome(y_test, ypred, 'SVM Regressor')
-    print_regression_residuals(y_test, ypred, 'SVM Regressor')
+    plot_regression_outcome(y_test, ypred, 'GBR Regressor')
+    print_regression_residuals(y_test, ypred, 'GBR Regressor')
 
-    joblib.dump(final_model, '../models/SVM_regressor.pkl')
+    joblib.dump(final_model, '../models/GBR_regressor.pkl')
 
 
-    # SVR_search = {
-    #     'model': space.Categorical([SVR()]),
-    #     'model__C': space.Real(40, 60, name='C'),
-    #     'model__epsilon': space.Real(0.02,  0.2, name='epsilon'),
-    #     'model__gamma': space.Categorical(['auto', 'scale'], name='gamma'),
-    #     'model__tol': space.Real(0.00005, 0.001, name='tol')
-    # }
-    #
-    # pipe = Pipeline([
-    #     ('model', SVR())
-    # ])
-    # res_bp = BayesSearchCV(pipe, [(SVR_search, 40)], cv=3)
-    # res_bp.fit(x_train, y_train)
-    #
-    # params = dict(res_bp.best_params_)
-    #
-    # best_parameters = params['model'].get_params()
-    # print(best_parameters)
-    # compare_trends(x_train, y_train, x_test, y_test)
 
 
 
