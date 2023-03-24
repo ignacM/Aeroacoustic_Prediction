d7d70eb20e6f8459607152fff647e1bfa79d154e
ignacM <nacho.moral.a@gmail.com>
2023-03-24 04:51:58 +0000

    Added comparison between different regressors

--- src/validation.py
+++ src/validation.py
@@ -3,15 +3,16 @@ import numpy as np
 import joblib
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from train_test_split import deNormalize
-from functions import regression_eval as re
+from functions import regression_eval as reval
 
 
 
 if __name__ == '__main__':
 
-
     # Load model:
     regressor = joblib.load('../models/SVM_regressor.pkl')
+    # regressor = joblib.load('../models/GPR_regressor.pkl')
+    # regressor = joblib.load('../models/GBR_regressor.pkl')
     df = pd.read_csv('../Data/dec22/x_validation.csv')
 
     # Scale data
@@ -19,6 +20,7 @@ if __name__ == '__main__':
     df = scaler.fit_transform(df)
 
     predictions = []
+    dicta = {}
     predictions_name =\
         [
             '7.5 degrees', '22.5 degrees', '37.5 degrees', '52.5 degrees', '67.5 degrees', '82.5 degrees',
@@ -32,57 +34,21 @@ if __name__ == '__main__':
         print(x_val)
 
         ypred = regressor.predict(x_val)
+        # Models have been trained with log scale dB data, reversing log:
         ypred = np.exp(ypred)
+        dicta[predictions_name[degree]] = ypred
         predictions.append(ypred)
 
 
-
-print('These are the preditcions:', predictions)
-print(len(predictions))
-
-
-re.print_predictions(predictions, predictions_name)
+# print('These are the preditcions:', predictions)
 
 
 
-# def dataSplit(exclude=60, scaler=MinMaxScaler()):
-#     """
-#     Splits data into train and test data.
-#     :param exclude: Angle to be excluded from dataset: [0, 15, 30, ... 150, 165]
-#     :param scaler: select scaler from sklearn.preprocessing
-#     :return: x_train, y_train, x_test, y_test,
-#             min, max (min and max returned for descaling)
-#     """
-#     # Dividing data between X and Y
-#     path = '..\Data\dec22\data_input.xlsx'
-#     # Sheet name either azi or pol
-#     df = pd.read_excel(path, sheet_name='azi')
-#     minimum = min(df['db'])
-#     maximum = max(df['db'])
-#
-#     # Selecting which set to use as test data
-#     exclude = exclude
-#     df['exclude'] = 0
-#     df.loc[(df['degree'] == exclude), 'exclude'] = 1
-#     exclude_col = df.loc[:, ['exclude']]
-#
-#
-#
-#     scaler = scaler
-#     df = scaler.fit_transform(df)
-#     df = pd.DataFrame(df, columns=['db', 'theta', 'degree', 'exclude'])
-#     df['exclude'] = exclude_col
-#
-#     Y_data = df.drop(['theta', 'degree'], axis=1)
-#
-#     X_data = df.drop(['db'], axis=1)
-#     X_data = X_data.to_numpy()
-#
-#     x_train = X_data[(X_data[:, -1] == 0), 0:2]
-#     x_test = X_data[(X_data[:, -1] == 1), 0:2]
-#
-#     y_train = Y_data.loc[(Y_data['exclude'] == 0), 'db']
-#     y_test = Y_data.loc[(Y_data['exclude'] == 1), 'db']
-#
-#     return x_train, y_train, x_test, y_test, minimum, maximum
+reval.print_predictions(predictions, predictions_name)
+
+predictions = []
+predictions.append(dicta['22.5 degrees'])
+# predictions.append(dicta['82.5 degrees'])
+reval.print_predictions(predictions, ['22.5 degrees'])
+# reval.print_predictions(predictions, ['22.5 degrees', '82.5 degrees'])
 
